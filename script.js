6e1b53980a8c3c8a47ba625fde69168b614f018d
fashionpolice <60684862+tooweaknotice@users.noreply.github.com>
2023-10-31 13:28:56 -0700

    Add preliminary RPS code
    Add preliminary RPS code

---
+++ script.js
@@ -0,0 +1,69 @@
+/*
+GET player selection of RPS.
+GENERATE a CPU RPS selection.
+IF player selection > CPU selection
+  PRINT message declaring user is winner.
+ELSE
+  PRINT message declaring user is loser.
+*/
+
+
+function getComputerChoice() {
+  return Math.floor(Math.random() * 2);  
+}
+
+function getPlayerChoice() {
+  while (keepGoing === true){
+    let playerInput = prompt('What is your choice?');
+    switch (playerInput.toLowerCase()){
+      case 'rock':
+        return 0;
+      case 'paper':
+        return 1;
+      case 'scissors':
+        return 2;
+      default:
+        console.log(playerInput + ' is not a valid choice.')
+    }
+  }
+
+}
+
+function decodeChoice(numChoice){
+  switch (num){
+    case 0:
+      return 'Rock';
+    case 1:
+      return 'Paper';
+    case 2:
+      return 'Scissors';
+    default:
+      return 'Invalid';
+  }
+}
+
+function getWinner(playerChoice, computerChoice){
+  let winner = playerChoice - computerChoice;
+  if (winner === -2) {
+    winner = 1;
+  }
+  return winner;
+}
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = getPlayerChoice();
+  computerSelection = getComputerChoice();
+  let winner = getWinner(playerSelection,computerSelection);
+  switch (winner){
+    case -1:
+      console.log('You Lose! ' + decodeChoice(computerSelection) + ' beats ' + decodeChoice(playerSelection));
+      break;
+    case 0:
+      console.log('Draw!');
+      break;
+    case 1:
+      console.log('You Win! ' + decodeChoice(playerSelection) + ' beats ' + decodeChoice(computerSelection));
+      break;
+  }
+  return winner;
+}
